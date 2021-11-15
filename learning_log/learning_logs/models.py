c6994e857a56f273467b85b0a8b0140649096636
andr <kli4ev2015@yandex.ru>
2021-11-15 21:21:42 +0300

    Add

--- learning_log/learning_logs/models.py
+++ learning_log/learning_logs/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Topic(models.Model):
+    """Тема которую изучает пользователь."""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Возвращает строковое представление модели."""
+        return self.text
+
