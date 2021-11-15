35f1d400d60b690aae6ec3480b8739a5ddf17391
andr <kli4ev2015@yandex.ru>
2021-11-15 23:31:30 +0300

    Add

--- learning_log/learning_logs/models.py
+++ learning_log/learning_logs/models.py
@@ -10,3 +10,16 @@ class Topic(models.Model):
         """Возвращает строковое представление модели."""
         return self.text
 
+
+class Entry(models.Model):
+    """Информация, изученная пользователем по теме """
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Возвращает строковое представление модели"""
+        return f"{self.text[:50]}..."
