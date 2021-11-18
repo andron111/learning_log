9bcc71797aa2fcac19c8225c1e0980c733758a88
andr <kli4ev2015@yandex.ru>
2021-11-18 21:22:08 +0300

    Add

--- meal_planner/meal_planner/meal_plans/urls.py
+++ meal_planner/meal_planner/meal_plans/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path
 from . import views
 
-app_name = 'learning_logs'
+app_name = 'meal_plans'
 urlpatterns = [
     # Домашняя страница
     path('', views.index, name='index'),
--- meal_planner/meal_planner/meal_plans/views.py
+++ meal_planner/meal_planner/meal_plans/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 def index(request):
     """Домашняя страница приложения learning log."""
-    return render(request, 'learning_logs/index.html')
+    return render(request, 'meal_plans/index.html')
 # Create your views here.
