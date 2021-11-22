67cf41877a22730683e260eed197f1f5a9a450b4
Web_1 <kli4ev2015@yandex.ru>
2021-11-22 13:33:32 +0300

    URL scheme for individual topics

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -6,4 +6,8 @@ app_name = 'learning_logs'
 urlpatterns = [
     # Домашняя страница
     path('', views.index, name='index'),
+    # Страница со списком всех тем.
+    path('topics/', views.topics, name='topics'),
+    # Страница с подробной инфой по отдельной теме.
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
+from .models import Topic
 
 
 def index(request):
     """Домашняя страница приложения learning log."""
     return render(request, 'learning_logs/index.html')
-# Create your views here.
+
+
+def topics(request):
+    """Вывод список тем."""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+
+def topic(request, topic_id):
+    """Выводит одну тему и все её записи."""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
+
